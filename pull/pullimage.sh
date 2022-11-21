e66b671f0f3183623ddfa58bb97b9fb5a5ffdaef
liusky0212 <50815154+liusky0212@users.noreply.github.com>
2022-11-21 16:11:01 +0800

    Create pullimage.sh

---
+++ pull/pullimage.sh
@@ -0,0 +1,17 @@
+imagestore="/tmp/images/"
+dockerRegistry="harbor.dev.21vianet.com/cube-studio/"
+
+mkdir -p $imagestore
+
+for i in `cat list`;
+do image=$i;
+echo "开始拉取镜像：$image"
+docker pull $image
+
+echo "更换标签"
+imageString=${image##*\/};
+echo $dockerRegistry$imageString
+docker tag $i $dockerRegistry$imageString
+echo "打包镜像"
+docker save -o  $imagestore$imageString.tar $dockerRegistry$imageString
+done
